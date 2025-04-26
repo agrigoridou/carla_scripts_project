00b93ded3c59ff90495c995f5d83ad86fd0ec816
Grigoridou Athanasia <167294620+agrigoridou@users.noreply.github.com>
2025-04-26 23:37:31 +0300

    Update list_available_vehicles.py

--- list_available_vehicles.py
+++ list_available_vehicles.py
@@ -2,13 +2,18 @@
 
 import carla
 
-client = carla.Client("localhost", 2000)
-client.set_timeout(10.0)
+def list_available_vehicles():
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(5.0)
+    world = client.get_world()
+    blueprint_library = world.get_blueprint_library()
 
-# Απόκτηση του κόσμου και της βιβλιοθήκης blueprints
-world = client.get_world()
-blueprint_library = world.get_blueprint_library()
+    # Φιλτράρουμε ΜΟΝΟ τα οχήματα
+    vehicles = blueprint_library.filter('vehicle.*')
 
-# Εκτύπωση όλων των διαθέσιμων οχημάτων
-for blueprint in blueprint_library.filter('vehicle.*'):
-    print(blueprint.id)
+    print(f"Σύνολο διαθέσιμων οχημάτων: {len(vehicles)}")
+    for vehicle in vehicles:
+        print(vehicle.id)
+
+if __name__ == '__main__':
+    list_available_vehicles()
